f00ed0e56b828823c3b16c4eaa5e500f483faec3
Pedro  Emmanuel G. C. Machado <132278032+Pedro-Emmanuel-G-C-Machado@users.noreply.github.com>
2024-11-28 13:00:22 -0300

    Finalizando Lista circular e corrigindo algumas coisas

--- src/estruturaDeDados/Lista.java
+++ src/estruturaDeDados/Lista.java
@@ -3,7 +3,7 @@ package estruturaDeDados;
 public class Lista {
 
     private No cabeca;
-    
+
     public Lista() {
         this.cabeca = null;
     }
@@ -12,115 +12,105 @@ public class Lista {
         No n = new No(i);
         n.setProximo(cabeca);
         cabeca = n;
-
     }
 
     public boolean buscaElemento(int i) {
-        if(cabeca == null){
-            cabeca = new No(i);
-            return false;       
-        }
         No n = cabeca;
-        while(n.getProximo() != null){
-            if(n.getValor() == i){
+        while (n != null) {
+            if (n.getValor() == i) {
                 return true;
             }
+            n = n.getProximo();
         }
-        return  false;
-         }
+        return false;
+    }
 
-
-
-    public Object buscaIndice(int i) {
-              if(cabeca == null){
-                return -1;
-                }
-                No n = cabeca;
-                int indicie = 0;
-                while(n.getProximo() != null){
-                    if (n.getValor() == i) {
-                        return indicie;
-
-                    }
-                    n = n.getProximo();
-                    indicie++;
-
-                } 
-                return -1;
-                }
-
-    public void insereFim(int i) {
-                if(cabeca == null){
-            cabeca = new No(i);
-            return;       
+    public int buscaIndice(int i) {
+        if (cabeca == null) {
+            return -1;
         }
         No n = cabeca;
-        while(n.getProximo() != null){
-            n.getProximo();
+        int indice = 0;
+        while (n != null) {
+            if (n.getValor() == i) {
+                return indice;
+            }
+            n = n.getProximo();
+            indice++;
         }
-        No prox = new No(i);
-        n.setProximo(prox);      
+        return -1;
+    }
 
+    public void insereFim(int i) {
+        No novoNo = new No(i);
+        if (cabeca == null) {
+            cabeca = novoNo;
+            return;
+        }
+        No n = cabeca;
+        while (n.getProximo() != null) {
+            n = n.getProximo();
+        }
+        n.setProximo(novoNo);
     }
 
     public void removeInicio() {
-        if(cabeca != null){
-           cabeca =  cabeca.getProximo();
+        if (cabeca != null) {
+            cabeca = cabeca.getProximo();
         }
     }
 
     public void removeFim() {
-        if(cabeca == null){
+        if (cabeca == null) {
+            return;
+        }
+        if (cabeca.getProximo() == null) {
             cabeca = null;
             return;
         }
-        if(cabeca.getProximo() == null){
-            cabeca =null;
-            return;  
-        }
         No n = cabeca;
-        while(n.getProximo().getProximo() != null){
+        while (n.getProximo().getProximo() != null) {
             n = n.getProximo();
         }
         n.setProximo(null);
     }
+
     public void removeIndice(int i) {
-        if(cabeca == null || i < 0 ){
+        if (cabeca == null || i < 0) {
             return;
         }
-        if(i == 0){
-            cabeca.getProximo();
+        if (i == 0) {
+            cabeca = cabeca.getProximo();
+            return;
         }
         No n = cabeca;
-    for (int j = 0; j < i - 1; j++) {
-        n = n.getProximo();
-  }
-        if(n == null || n.getProximo() == null){
+        for (int j = 0; n != null && j < i - 1; j++) {
+            n = n.getProximo();
+        }
+        if (n == null || n.getProximo() == null) {
             return;
-        }    
+        }
         n.setProximo(n.getProximo().getProximo());
     }
 
     public void insereElementoPosicao(int i, int j) {
-    if (i < 0) {
-        return;
+        if (i < 0) {
+            return;
+        }
+        No novoNo = new No(j);
+        if (i == 0) {
+            novoNo.setProximo(cabeca);
+            cabeca = novoNo;
+            return;
+        }
+        No n = cabeca;
+        for (int k = 0; n != null && k < i - 1; k++) {
+            n = n.getProximo();
+        }
+        if (n == null) {
+            return;
+        }
+        novoNo.setProximo(n.getProximo());
+        n.setProximo(novoNo);
     }
-    No novoNo = new No(j);
-    if (i == 0) {
-        novoNo.setProximo(cabeca);
-        cabeca = novoNo;
-        return;
-    }
-    No n = cabeca;
-    for (int k = 0; n != null && k < i - 1; k++) {
-        n = n.getProximo();
-    }
-    if (n == null) {
-        return;
-    }
-    novoNo.setProximo(n.getProximo());
-    n.setProximo(novoNo);
-}
-
-    
 }
--- src/estruturaDeDados/ListaCircular.java
+++ src/estruturaDeDados/ListaCircular.java
@@ -1,49 +1,157 @@
 package estruturaDeDados;
 
 public class ListaCircular {
+    private No cabeca;
+    private No cauda;
 
     public ListaCircular() {
-        // TODO Auto-generated constructor stub
-        throw new UnsupportedOperationException("Unimplemented constructor 'ListaCircular'");
+        this.cabeca = null;
+        this.cauda = null;
     }
 
     public void insereInicio(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insereInicio'");
+        No n = new No(i);
+        if (n == null) {
+            cabeca = n;
+            cauda = n;
+            cauda.setProximo(cabeca);
+        } else {
+            n.setProximo(cabeca);
+            cauda.setProximo(n);
+            cabeca = n;
+            ;
+
+        }
     }
 
     public boolean buscaElemento(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscaElemento'");
+        if (cabeca == null) {
+            return false;
+        }
+        No n = cabeca;
+        do {
+            if (n.getValor() == i) {
+                return true;
+            }
+            n = n.getProximo();
+        } while (n != cabeca);
+        return false;
     }
 
-    public Object buscaIndice(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscaIndice'");
+    public int buscaIndice(int i) {
+        if (cabeca == null) {
+            return -1;
+        }
+        No n = cabeca;
+        int indice = 0;
+        do {
+            if (n.getValor() == i) {
+                return indice;
+            }
+            n = n.getProximo();
+            indice++;
+        } while (n != cabeca);
+        return -1;
     }
 
     public void insereFim(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insereFim'");
+        No n = new No(i);
+        if (cabeca == null) {
+            cabeca = n;
+            cauda = n;
+            cauda.setProximo(cabeca);
+        } else {
+            cauda.setProximo(n);
+            cauda = n;
+            cauda.setProximo(cabeca);
+        }
     }
 
     public void removeInicio() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeInicio'");
+        if (cabeca == null) {
+            return;
+        }
+        if (cabeca == cauda) {
+            cabeca = null;
+            cauda = null;
+        } else {
+            cabeca = cabeca.getProximo();
+            cauda.setProximo(cabeca);
+
+        }
     }
 
     public void removeFim() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeFim'");
+        if (cabeca == null) {
+            return;
+        }
+        if (cabeca == cauda) {
+            cabeca = null;
+            cauda = null;
+
+        }
+        No n = cabeca;
+        while (n.getProximo() != cauda) {
+            n = n.getProximo();
+        }
+        n.setProximo(cabeca);
+        cauda = cabeca;
     }
 
     public void removeIndice(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeIndice'");
+
+        int indice = 0;
+        if (cabeca == null || indice < 0) {
+            return;
+        }
+        if (indice == 0) {
+            removeInicio();
+            return;
+        }
+        No n = cabeca;
+        for (int j = 0; j < i - 1; j++) {
+            if (n.getProximo() == null || n.getProximo() == cabeca) {
+                return;
+            }
+            n = n.getProximo();
+        }
+        if (n.getProximo() == cabeca) {
+            return;
+        }
+        n.setProximo(n.getProximo().getProximo());
+        if (n.getProximo() == cabeca) {
+            cauda = n;
+        }
     }
 
-    public void insereElementoPosicao(int i, int j) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insereElementoPosicao'");
+    public void insereElementoPosicao(int pos, int valor) {
+        if (pos < 0) {
+            return;
+        }
+        No novoNo = new No(valor);
+        if (pos == 0) {
+            if (cabeca == null) {
+                cabeca = novoNo;
+                cauda = novoNo;
+                cauda.setProximo(cabeca);
+            } else {
+                novoNo.setProximo(cabeca);
+                cabeca = novoNo;
+                cauda.setProximo(cabeca);
+            }
+            return;
+        }
+        No n = cabeca;
+        for (int i = 0; n != null && i < pos - 1; i++) {
+            n = n.getProximo();
+        }
+        if (n == null) {
+            return;
+        }
+        novoNo.setProximo(n.getProximo());
+        n.setProximo(novoNo);
+        if (novoNo.getProximo() == cabeca) {
+            cauda = novoNo;
+        }
     }
 }
